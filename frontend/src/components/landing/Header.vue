f30560ebfa746b0739d80b92c6fb8bb925ca7482
ngracioli <nicolas.gracioli@gmail.com>
2025-08-20 21:55:00 -0300

    criado protótipo de navbar

---
+++ frontend/src/components/landing/Header.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+    <header class="bg-white shadow-xl py-2 fixed w-full">
+        <nav class="flex items-center justify-evenly p-4">
+            <h1 class="text-4xl font-semibold text-zinc-800">Ecoply</h1>
+            <ul class="flex space-x-4 text-xl">
+                <li>
+                    <a href="#" class="text-zinc-600 hover:text-zinc-800"
+                        >Home</a
+                    >
+                </li>
+                <li>
+                    <a href="#" class="text-zinc-600 hover:text-zinc-800"
+                        >About</a
+                    >
+                </li>
+                <li>
+                    <a href="#" class="text-zinc-600 hover:text-zinc-800"
+                        >Services</a
+                    >
+                </li>
+                <li>
+                    <a href="#" class="text-zinc-600 hover:text-zinc-800"
+                        >Contact</a
+                    >
+                </li>
+            </ul>
+            <button class="bg-green-600 text-white px-4 py-2 rounded">
+                Get Started
+            </button>
+        </nav>
+    </header>
+</template>
+
+<style scoped></style>
