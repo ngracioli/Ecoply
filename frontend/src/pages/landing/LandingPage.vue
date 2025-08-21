f17e24e49f0af6cc6522c011dd64fe3e704f26d2
ngracioli <nicolas.gracioli@gmail.com>
2025-08-20 21:54:28 -0300

    criado a page LandingPage.vue com componentes bases

---
+++ frontend/src/pages/landing/LandingPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from "../../components/landing/Header.vue";
+import Hero from "../../components/landing/Hero.vue";
+</script>
+
+<template>
+    <Header />
+    <Hero />
+</template>
+
+<style scoped></style>
