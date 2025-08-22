a336c96cce8d3cec5db5d4acc637fcb607f38e42
ngracioli <nicolas.gracioli@gmail.com>
2025-08-22 20:22:18 -0300

    refatoração no tailwindcss

--- frontend/src/components/landing/Hero.vue
+++ frontend/src/components/landing/Hero.vue
@@ -1,39 +1,34 @@
 <script setup lang="ts"></script>
 
 <template>
-    <main
-        class="flex flex-col items-center justify-center h-screen text-center"
-    >
-        <h3
-            class="text-2xl font-bold text-green-500 mb-2 flex items-center gap-2"
-        >
-            Energia Renovável
-            <i class="pi pi-check-circle"></i>
-        </h3>
-        <h1 class="text-6xl font-extrabold text-neutral-700 mb-6">
-            Transforme <span class="text-yellow-600">energia excedente</span
-            ><br />
-            em
-            <span class="text-green-600">oportunidade</span>
-        </h1>
-        <h2 class="text-3xl font-bold text-neutral-600 mb-10">
-            O Ecoply conecta quem gera energia limpa a quem<br />precisa dela,
-            de forma transparente e direta.
-        </h2>
-        <section class="flex flex-row gap-8 text-2xl font-semibold">
-            <button
-                class="bg-green-600 text-white px-8 py-4 rounded-2xl flex items-center gap-4"
-            >
-                Explorar as Ofertas
-                <i class="pi pi-arrow-right text-white"></i>
-            </button>
-            <button
-                class="bg-transparent border-2 border-yellow-600 text-yellow-600 px-4 py-2 rounded-2xl"
-            >
-                Saiba Mais
-            </button>
-        </section>
-    </main>
+  <main class="flex h-screen flex-col items-center justify-center text-center">
+    <h3 class="mb-2 flex items-center gap-2 text-2xl font-bold text-green-500">
+      Energia Renovável
+      <i class="pi pi-check-circle"></i>
+    </h3>
+    <h1 class="mb-6 text-6xl font-extrabold text-neutral-700">
+      Transforme <span class="text-yellow-600">energia excedente</span><br />
+      em
+      <span class="text-green-600">oportunidade</span>
+    </h1>
+    <h2 class="mb-10 text-3xl font-bold text-neutral-600">
+      O Ecoply conecta quem gera energia limpa a quem<br />precisa dela, de
+      forma transparente e direta.
+    </h2>
+    <section class="flex flex-row gap-8 text-2xl font-semibold">
+      <button
+        class="flex items-center gap-4 rounded-2xl bg-green-600 px-8 py-4 text-white"
+      >
+        Explorar as Ofertas
+        <i class="pi pi-arrow-right text-white"></i>
+      </button>
+      <button
+        class="rounded-2xl border-2 border-yellow-600 bg-transparent px-4 py-2 text-yellow-600"
+      >
+        Saiba Mais
+      </button>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
