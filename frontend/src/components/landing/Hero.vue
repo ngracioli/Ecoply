9c879639e0bd5160663d8c0646f8d0b066d61afe
ngracioli <nicolas.gracioli@gmail.com>
2025-08-22 22:02:44 -0300

    refatoração de classes de cores personalizadas

--- frontend/src/components/landing/Hero.vue
+++ frontend/src/components/landing/Hero.vue
@@ -2,14 +2,17 @@
 
 <template>
   <main class="flex h-screen flex-col items-center justify-center text-center">
-    <h3 class="mb-2 flex items-center gap-2 text-2xl font-bold text-green-500">
+    <h3
+      class="text-primary-color mb-2 flex items-center gap-2 text-2xl font-bold"
+    >
       Energia Renovável
       <i class="pi pi-check-circle"></i>
     </h3>
     <h1 class="mb-6 text-6xl font-extrabold text-neutral-700">
-      Transforme <span class="text-yellow-600">energia excedente</span><br />
+      Transforme <span class="text-secondary-color">energia excedente</span
+      ><br />
       em
-      <span class="text-green-600">oportunidade</span>
+      <span class="text-primary-color">oportunidade</span>
     </h1>
     <h2 class="mb-10 text-3xl font-bold text-neutral-600">
       O Ecoply conecta quem gera energia limpa a quem<br />precisa dela, de
@@ -17,13 +20,13 @@
     </h2>
     <section class="flex flex-row gap-8 text-2xl font-semibold">
       <button
-        class="flex items-center gap-4 rounded-2xl bg-green-600 px-8 py-4 text-white"
+        class="bg-accent-color flex items-center gap-4 rounded-2xl px-8 py-4 text-white"
       >
         Explorar as Ofertas
         <i class="pi pi-arrow-right text-white"></i>
       </button>
       <button
-        class="rounded-2xl border-2 border-yellow-600 bg-transparent px-4 py-2 text-yellow-600"
+        class="border-secondary-color text-secondary-color rounded-2xl border-2 bg-transparent px-4 py-2"
       >
         Saiba Mais
       </button>
