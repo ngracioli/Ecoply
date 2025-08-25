bc6c5a98da1e90cfc2ce86394f5f3403fc05a05d
henriqueoz <pedroozorio30@gmail.com>
2025-08-24 22:04:52 -0300

    wip: testing postgres connection

--- back/entrypoint/main.go
+++ back/entrypoint/main.go
@@ -3,14 +3,59 @@ package main
 import (
 	"ecoply/internal/config"
 	"ecoply/internal/server"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
 	loadEnvironment()
+
+	var cfg *config.Config = config.GetConfig()
+	var dsn string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s",
+		cfg.DBHost, cfg.DBUsername, cfg.DBPassword, cfg.DBDatabase, cfg.DBPort, cfg.DBTimezone,
+	)
+	con, err := gorm.Open(postgres.Open(dsn))
+	fmt.Printf("%v\n", err)
+
+	db, err := con.DB()
+	fmt.Printf("%v\n", err)
+
+	var queries []string = []string{
+		"CREATE TABLE IF NOT EXISTS users (id BIGINT GENERATED ALWAYS AS IDENTITY, name VARCHAR(255) NOT NULL)",
+		"INSERT INTO users (name) VALUES ('BANANA')",
+		"SELECT * FROM users",
+	}
+
+	for _, q := range queries {
+		rows, err := db.Query(q)
+		if err != nil {
+			fmt.Printf("Query error: %v\n", err)
+			continue
+		}
+		defer rows.Close()
+
+		if rows.Next() {
+			var user struct {
+				Id   uint64
+				Name string
+			}
+			err := rows.Scan(&user.Id, &user.Name)
+			if err != nil {
+				fmt.Printf("Scan error: %v\n", err)
+			} else {
+				fmt.Printf("User: %+v\n", user)
+			}
+		}
+	}
+
+	os.Exit(0)
+
 	server.NewAndRun()
 }
 
