11a24ccc40cc7bf8d841fa96daef11b73dd744d0
ngracioli <nicolas.gracioli@gmail.com>
2025-09-01 20:47:41 -0300

    criação de protóripo da dashboard

---
+++ frontend/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SideBarDashboard from "../../components/dashboard/SideBarDashboard.vue";
+</script>
+
+<template>
+  <main class="flex h-screen w-full bg-neutral-700">
+    <SideBarDashboard />
+  </main>
+</template>
+
+<style scoped></style>
