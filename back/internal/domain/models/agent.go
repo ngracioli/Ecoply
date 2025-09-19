2cbd31ab299e5a3b8758833a8896cee830fc3fb9
ozorio.pedro <ozorio.pedro@crmsolucoes.com.br>
2025-09-19 16:39:36 -0300

    refactor(models): add relations gorm tag

--- back/internal/domain/models/agent.go
+++ back/internal/domain/models/agent.go
@@ -9,9 +9,9 @@ type Agent struct {
 	CompanyName string `gorm:"type:varchar(255);not null"`
 	CceeCode    string `gorm:"type:varchar(50);not null;uniqueIndex"`
 
-	SubmarketId uint `gorm:"references:ID;not null"`
-	Submarket   Submarket
+	SubmarketId uint      `gorm:"references:ID;not null"`
+	Submarket   Submarket `gorm:"foreignKey:SubmarketId"`
 
-	AddressId uint `gorm:"references:ID;not null"`
-	Address   Address
+	AddressId uint    `gorm:"references:ID;not null"`
+	Address   Address `gorm:"foreignKey:AddressId"`
 }
