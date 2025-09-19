d3c1c2552097f2b85486e91f018493ff900392f6
ozorio.pedro <ozorio.pedro@crmsolucoes.com.br>
2025-09-19 16:58:24 -0300

    feat(model): update offer and purchase models

--- back/internal/domain/models/offer.go
+++ back/internal/domain/models/offer.go
@@ -23,12 +23,13 @@ type Offer struct {
 	InitialQuantityMwh   float64 `gorm:"type:decimal(10,3);not null"`
 	RemainingQuantityMwh float64 `gorm:"type:decimal(10,3);not null"`
 
-	EnergyType string `gorm:"type:varchar(50);not null"`
-
 	PeriodStart string `gorm:"type:date;not null"`
 	PeriodEnd   string `gorm:"type:date;not null"`
 
 	Status string `gorm:"type:varchar(50);not null"`
+
+	EnergyTypeId uint       `gorm:"references:ID;not null"`
+	EnergyType   EnergyType `gorm:"foreignKey:EnergyTypeId"`
 
 	SubmarketId uint `gorm:"references:ID;not null"`
 	Submarket   Submarket
