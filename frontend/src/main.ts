45a70b0ed130c946817a7e2506a7a8a6b95c1193
ngracioli <nicolas.gracioli@gmail.com>
2025-10-07 20:38:08 -0300

    adiciona o sigma primevue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,10 +4,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+
 const app = createApp(App);
 
 app.use(router);
 
 app.use(store);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
 
 app.mount("#app");
