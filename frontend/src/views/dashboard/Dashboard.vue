85870eed209f3ec6dfac0830c21c0c5a24899f63
ngracioli <nicolas.gracioli@gmail.com>
2025-10-07 19:41:06 -0300

    reset dashboard antiga

--- frontend/src/views/dashboard/Dashboard.vue
+++ frontend/src/views/dashboard/Dashboard.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import SideBarDashboard from "../../components/dashboard/SideBarDashboard.vue";
-import TopBarDashboard from "../../components/dashboard/TopBarDashboard.vue";
-
-const isSidebarOpen = ref(true);
+import DashboardMain from "./pages/DashboardMain.vue";
 </script>
 
 <template>
   <main class="flex h-screen w-full bg-neutral-700">
-    <SideBarDashboard v-if="isSidebarOpen" />
-    <div class="flex flex-1 flex-col">
-      <TopBarDashboard v-model:sidebarOpen="isSidebarOpen" />
-      <div class="flex-1 p-6 text-gray-800">
-        <p>Bem-vindo ao seu dashboard!</p>
-      </div>
-    </div>
+    <DashboardMain />
   </main>
 </template>
 
---
+++ frontend/src/views/dashboard/pages/DashboardMain.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <v-btn :disabled="true" color="primary"> Button </v-btn>
+</template>
+
+<style scoped></style>
