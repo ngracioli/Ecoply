4d7e41a3e572ba8bbf5a01e982ed96367b9cdb5d
ngracioli <nicolas.gracioli@gmail.com>
2025-11-26 21:18:02 -0300

    FINALMENTE OFERTA

---
+++ frontend/src/views/landing/LandingPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Header from "../../components/landing/Header.vue";
+import Hero from "../../components/landing/Hero.vue";
+import HowItWorks from "../../components/landing/HowItWorks.vue";
+import BenefitsSection from "../../components/landing/BenefitsSection.vue";
+import OfferDemonstrationSection from "../../components/landing/OfferDemonstrationSection.vue";
+import JoinUsSection from "../../components/landing/JoinUsSection.vue";
+import FooterLanding from "../../components/landing/FooterLanding.vue";
+</script>
+
+<template>
+  <Header />
+  <Hero />
+  <HowItWorks />
+  <BenefitsSection />
+  <OfferDemonstrationSection />
+  <JoinUsSection />
+  <FooterLanding />
+</template>
+
+<style scoped></style>
