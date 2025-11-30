6a7576661b5a6b28b825d69b663556919efca102
ngracioli <nicolas.gracioli@gmail.com>
2025-11-30 16:19:20 -0300

    fim da config de deploy

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export const AUTH_TOKEN_KEY = "token";
